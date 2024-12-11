6ec9f83b8eb188e1b1d96c692bbf207bee92580b
DESKTOP-QO5VTJO\plame <plamemee2015@gmail.com>
2024-12-11 15:34:10 +0700

    no message

---
+++ pages/dashboard/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../../plugins/bootstrap-5/css/bootstrap.min.css">
+
+    <style>
+        body {
+            overflow-x: hidden;
+            overflow-y: scroll;
+            scrollbar-width: none;
+            -ms-overflow-style: none;
+            padding: 2px;
+        }
+
+        ::-webkit-scrollbar {
+            width: 0px;
+            background: transparent;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: transparent;
+        }
+
+        .header {
+            font-size: 5rem;
+            font-weight: 800;
+            text-transform: uppercase;
+            text-align: center;
+            color: red;
+
+        }
+    </style>
+</head>
+<body>
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="header">
+                                <p>BOXING</p>
+                            </div>
+                            <!-- INPUT and TABLE Section -->
+                            <div class="row gx-3 gy-3">
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+
+                                            <!-- ส่วนที่ต้องแก้ -->
+                                            <h1 class="text-center">INPUT</h1>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+
+                                            <!-- ส่วนที่ต้องแก้ -->
+                                            <h1 class="text-center">TABLE</h1>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- OUTPUT Section -->
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+
+                                            <!-- ส่วนที่ต้องแก้ -->
+                                            <h1 class="text-center">OUTPUT</h1>
+
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="../../plugins/bootstrap-5/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
